3666d83eb3736d098960ee705e983a060da38027
codec-abc <noemail>
2016-12-04 17:31:54 +0100

    clean code

--- electronDigitReader/ElectronApp/unityFFIBridge.js
+++ electronDigitReader/ElectronApp/unityFFIBridge.js
@@ -25,14 +25,17 @@ window.bridge.HandleRGBTexture = function (pixel_array, width, height)
         throw msg;
     }
 
-    var buf = Buffer.alloc(width * height * 4);
-    var buf2 = Buffer.alloc(width * height * 3);
+    var float_array_to_send_texture_to_cntk = Buffer.alloc(width * height * 4);
+    var byte_array_to_save_texture_with_rust_dll = Buffer.alloc(width * height * 3);
     var float_array = [];
 
-    var z = 0;
-    var t = 0;
+
+    var float_array_as_string = "";
+    var rgb_texture_byte_index = 0;
+    var mono_texture_float_index = 0;
     for (var i = 0; i < width; i++)
     {
+        var current_texture_line_as_float = ""
         for (var j = height - 1; j >= 0; j--)
         {
             var sum = 0;
@@ -40,25 +43,34 @@ window.bridge.HandleRGBTexture = function (pixel_array, width, height)
             {
                 var index = (i * width * 3 + j * 3) + k;
                 sum += pixel_array[index];
-                buf2[z] = pixel_array[index];
-                z++;
+                byte_array_to_save_texture_with_rust_dll[rgb_texture_byte_index] = pixel_array[index];
+                rgb_texture_byte_index++;
             }
-            var pixel_value = 255.0 -(sum / 3.0);
-            buf.writeFloatLE(pixel_value, t * 4);
-            float_array.push(pixel_value);
-            t++;
+            var pixel_value = 255.0 - (sum / 3.0);
+            pixel_value = pixel_value * 3.0;
+            if (pixel_value > 255.0)
+            {
+                pixel_value = 255.0;
+            }
+            float_array_to_send_texture_to_cntk.writeFloatLE(pixel_value, mono_texture_float_index * 4);
+            float_array.push(float_array_to_send_texture_to_cntk.readFloatLE(mono_texture_float_index * 4));
+            current_texture_line_as_float += float_array[mono_texture_float_index] + " " ;
+            mono_texture_float_index++;
         }
+        float_array_as_string += current_texture_line_as_float + "\r\n";
     }
 
     var probas = Buffer.alloc(10 * 4);
-    var result = cntk_wrapper.analyze_digits(buf, probas);
+    var result = cntk_wrapper.analyze_digits(float_array_to_send_texture_to_cntk, probas);
 
     var probas_as_float = [];
     var max_proba = -999;
     var max_proba_digit = -1;
+    var stat = [];
 
     for (var i = 0; i < 10; i++)
     {
+        var obj = {};
         var proba = probas.readFloatLE(i * 4);
         probas_as_float.push(proba);
 
@@ -67,25 +79,16 @@ window.bridge.HandleRGBTexture = function (pixel_array, width, height)
             max_proba = proba;
             max_proba_digit = i;
         }
+        
+        obj.digit = i;
+        obj.proba = proba;
+        stat.push(obj);
     }
 
-    /*
-    var total = 0;
-    var new_msg = "";
-    for (var i = 0; i < width; i++)
-    {
-        var mid_string = ""
-        for (var j = 0; j < width; j++)
-        {
-            mid_string += float_array[total] + " " ;
-            total++;
-        }
-        new_msg += mid_string + "\r\n";
-    }
-    console.log(new_msg);
-    */
+    console.log(float_array_as_string);
+    console.log(stat);
     
     alert("most probable digit " + max_proba_digit);
 
-    // rust_wrapper.write_rgb_texture_byte_array_to_file(buf2, width * height * 3, width, height);
+    // rust_wrapper.write_rgb_texture_byte_array_to_file(byte_array_to_save_texture_with_rust_dll, width * height * 3, width, height);
 };
